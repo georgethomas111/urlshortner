7361bd2f01a3332c03a6df15e4731a1881d65b92
George Thomas <georgethomas.mec@gmail.com>
2017-05-24 23:06:54 -0700

    Small change for docker

--- main/main.go
+++ main/main.go
@@ -3,7 +3,7 @@ package main
 import "github.com/georgethomas111/urlshortner"
 
 func main() {
-	err := urlshortner.Start(":8080", "127.0.0.1", 5984, "tinyurl", "objectviews")
+	err := urlshortner.Start(":8080", "couchdb", 5984, "tinyurl", "objectviews")
 	if err != nil {
 		panic(err)
 	}
